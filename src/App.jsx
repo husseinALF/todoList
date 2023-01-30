406812826946e780e47fab0aa6612352dccb427c
Hussein Al Fartoussi <114392226+husseinALF@users.noreply.github.com>
2023-01-30 21:04:00 +0100

    klar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 
-let count = 1;
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodoTitle, setNewTodoTitle] = useState("");
